eff69723b3dab11475e7ddea01b1649705ca67af
Leandro C. Tomassini <leandroctomassini@gmail.com>
2022-05-18 18:11:44 -0300

    Google Login y archivos

--- helpers/google-verify.js
+++ helpers/google-verify.js
@@ -1,25 +1,22 @@
 const { OAuth2Client } = require('google-auth-library');
 
-const client = new OAuth2Client( process.env.GOOGLE_CLIENT_ID );
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 
-const googleVerify = async( idToken = '' ) => {
+async function googleVerify(token = '') {
 
-  const ticket = await client.verifyIdToken({
-      idToken,
-      audience: process.env.GOOGLE_CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
-      // Or, if multiple clients access the backend:
-      //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
-  });
+    const ticket = await client.verifyIdToken({
+        idToken: token,
+        audience: process.env.GOOGLE_CLIENT_ID,
+    });
 
-  const { name: nombre, 
-          picture: img, 
-          email: correo
-        } = ticket.getPayload();
-  
-  return { nombre, img, correo };
+    const { name, picture, email } = ticket.getPayload();
 
+    return {
+        nombre:name,
+        img: picture,
+        correo: email
+    }
 }
-
 
 module.exports = {
     googleVerify
